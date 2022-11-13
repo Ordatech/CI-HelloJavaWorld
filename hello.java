81b4b6f089144a910059a0830e90214a8157a909
Ordatech <Ordavid1045@gmail.com>
2022-11-13 18:41:05 +0200

    Adding..

--- hello.java
+++ hello.java
@@ -1,11 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i=0; i<=10; i++){
-<<<<<<< HEAD
-			System.out.println("Hello!.."+i);
-=======
-			System.out.println("Hello..."+i);
->>>>>>> 0d1c728ce76b94e38110ffd519a9db556399d763
+			System.out.println("**Hello..."+i);
 		}
 	}
 }
