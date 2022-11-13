0d1c728ce76b94e38110ffd519a9db556399d763
Ordatech <98533826+Ordatech@users.noreply.github.com>
2022-11-13 18:30:56 +0200

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,10 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i=0; i<=10; i++){
-			for(int j=0; j<=10; j++){
-				System.out.println("*"+i);
-				System.out.println("/"+j);
-			}
+			System.out.println("Hello..."+i);
 		}
 	}
 }
